50a58c8ed4866210ed30694a9eeb11240e813a96
hansieodendaal <pluto@tari.com>
2020-12-08 15:06:55 +0200

    Add consistent startup scripts
    This PR adds startup scripts for OSX and Ubuntu, similar to Windows,
    which are consistent with the updated runtime directory layout. It
    also adds 3rd party installation script for XMRig that will always
    install the latest version. Lastly installation tarball setup
    scripts will prepare a new zipped tarball for release, also
    according to the updated directory structure. Installed artefacts
    will now be similar accross OSX, Ubuntu and Windows.

---
+++ applications/tari_base_node/osx/runtime/start_tari_base_node.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+echo
+echo "Starting Base Node"
+echo
+
+# Initialize
+base_path="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+config_path="${base_path}/config"
+exe_path="${base_path}/runtime"
+open -a Terminal.app "${exe_path}/start_tor.sh"
+
+if [ ! -f "${config_path}/base_node_id.json" ] || [ ! -f "${config_path}/wallet_id.json" ]
+then
+    if [ ! -f "${config_path}/base_node_id.json" ]
+    then
+        echo Creating new "${config_path}/base_node_id.json";
+    fi
+    if [ ! -f "${config_path}/wallet_id.json" ]
+    then
+        echo Creating new "${config_path}/wallet_id.json";
+    fi
+    "${exe_path}/tari_base_node" --create_id --init --config "${config_path}/config.toml" --log_config "${config_path}/log4rs_base_node.yml" --base-path ${base_path}
+else
+    echo Using existing "${config_path}/base_node_id.json";
+    echo Using existing "${config_path}/wallet_id.json";
+fi
+if [ ! -f "${config_path}/log4rs_base_node.yml" ]
+then
+    echo Creating new "${config_path}/log4rs_base_node.yml";
+    "${exe_path}/tari_base_node" --init --config "${config_path}/config.toml" --log_config "${config_path}/log4rs_base_node.yml" --base-path ${base_path}
+else
+    echo Using existing "${config_path}/log4rs_base_node.yml";
+fi
+echo
+
+# Run
+echo Spawning Base Node into new terminal..
+open "${exe_path}/tari_base_node" --config "${config_path}/config.toml" --log_config "${config_path}/log4rs_base_node.yml" --base-path ${base_path} ${enable_mining}
+echo
---
+++ applications/tari_console_wallet/ubuntu/runtime/start_tari_console_wallet.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+echo
+echo "Starting Console Wallet"
+echo
+
+# Initialize
+base_path="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+config_path="${base_path}/config"
+exe_path="${base_path}/runtime"
+"${exe_path}/setup_tor-service.sh"
+
+if [ ! -f "${config_path}/console_wallet_id.json" ]
+then
+    echo Creating new "${config_path}/console_wallet_id.json";
+    "${exe_path}/tari_console_wallet" --create_id --init --config "${config_path}/config.toml" --log_config "${config_path}/log4rs_console_wallet.yml" --base-path ${base_path}
+else
+    echo Using existing "${config_path}/console_wallet_id.json";
+fi
+if [ ! -f "${config_path}/log4rs_console_wallet.yml" ]
+then
+    echo Creating new "${config_path}/log4rs_console_wallet.yml";
+    "${exe_path}/tari_console_wallet" --init --config "${config_path}/config.toml" --log_config "${config_path}/log4rs_console_wallet.yml" --base-path ${base_path}
+else
+    echo Using existing "${config_path}/log4rs_console_wallet.yml";
+fi
+echo
+
+# Run
+echo Spawning Console Wallet into new terminal..
+gnome-terminal --working-directory="$PWD" -- "${exe_path}/tari_console_wallet" --config "${config_path}/config.toml" --log_config "${config_path}/log4rs_console_wallet.yml" --base-path ${base_path}
+echo
